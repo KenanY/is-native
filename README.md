4ec50373527fa51b0343108db27d6471622b609b
Kenan Yildirim <kenan@kenany.me>
2014-04-28 10:10:10 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# is-native
+
+Lo-Dash's private `isNative` function as a component.
+
+## Installation
+
+``` bash
+$ component install KenanY/is-native
+```
+
+## API
+
+``` javascript
+var isNative = require('is-native');
+```
+
+### isNative(value)
+
+Checks if `value` is a native function.
---
+++ index.js
@@ -0,0 +1,20 @@
+var objectProto = Object.prototype;
+var toString = objectProto.toString;
+var reNative = RegExp('^' +
+  String(toString)
+    .replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+    .replace(/toString| for [^\]]+/g, '.*?') + '$'
+);
+
+/**
+ * Checks if `value` is a native function.
+ *
+ * @private
+ * @param {*} value The value to check.
+ * @returns {boolean} Returns `true` if the `value` is a native function, else `false`.
+ */
+function isNative(value) {
+  return typeof value == 'function' && reNative.test(value);
+}
+
+module.exports = isNative;
